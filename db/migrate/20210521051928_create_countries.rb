612861d0ff88991e766c3eaf16a94b08178a3eb6
Shannon M. Hauck <shannonmariehauck@gmail.com>
2021-05-21 01:21:59 -0400

    countries

---
+++ db/migrate/20210521051928_create_countries.rb
@@ -0,0 +1,12 @@
+class CreateCountries < ActiveRecord::Migration[6.0]
+  def change
+    create_table :countries do |t|
+      t.string :name, null: false, unique: true
+      t.string :two_letter_code, unique: true
+      t.string :three_letter_code, unique: true
+      t.references :global_area, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
