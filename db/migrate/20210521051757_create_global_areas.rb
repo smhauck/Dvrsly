b9001e573568c1f0ca113380f7cd146cf6077e85
Shannon M. Hauck <shannonmariehauck@gmail.com>
2021-05-21 01:21:24 -0400

    not null, unique

--- db/migrate/20210521051757_create_global_areas.rb
+++ db/migrate/20210521051757_create_global_areas.rb
@@ -1,8 +1,8 @@
 class CreateGlobalAreas < ActiveRecord::Migration[6.0]
   def change
     create_table :global_areas do |t|
-      t.string :name
-      t.boolean :active
+      t.string :name, null: false, unique: true
+      t.boolean :active, null: false, default: true
       t.text :description
 
       t.timestamps
