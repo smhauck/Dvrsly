4ff25fc4f4a22784b65e62dd1dfae82d29f5d03d
Shannon M. Hauck <shannonmariehauck@gmail.com>
2021-05-21 22:37:34 -0400

    pagination for users

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,7 +3,7 @@ class UsersController < ApplicationController
 
   # GET /users or /users.json
   def index
-    @users = User.all
+    @users = User.order(:display_name).page params[:page]
   end
 
   # GET /users/1 or /users/1.json
